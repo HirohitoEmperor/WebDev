7d71d9a4064ebd62624f5ff07662a2ac32e8718d
Hirohito <47021643+HirohitoEmperor@users.noreply.github.com>
2021-03-19 20:32:30 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 
-var myHeading = document.querySelector('h1');
-<<<<<<< HEAD
-myHeading.textContent = 'Hello Hello world!';
-=======
-myHeading.textContent = 'Hello world!2';
->>>>>>> Production1
+//var myHeading = document.querySelector('h1');
+//<<<<<<< HEAD
+//myHeading.textContent = 'Hello Hello world!';
+//=======
+//myHeading.textContent = 'Hello world!2';
+//>>>>>>> Production1
 
